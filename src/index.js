b5dad910e846a6e548a74a2ebec8020b975471f0
dimitricbrown <dimitricbrown@outlook.com>
2022-05-14 13:20:31 -0500

    Updated the index.js and created KandyKorner.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { KandyKorner } from "./components/KandyKorner"
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+export const API = "http://localhost:8088"
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <KandyKorner />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
